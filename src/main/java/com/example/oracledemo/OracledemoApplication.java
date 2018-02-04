17ea97f6933ddff9f82a1c47ff8b6734bca15cb3
pivotal-preston-sheldon <psheldon@pivotal.io>
2018-02-04 09:17:59 -0500

    Initial commit

---
+++ src/main/java/com/example/oracledemo/OracledemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.oracledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracledemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/oracledemo/OracledemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.oracledemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OracledemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
